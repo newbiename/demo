c88d8ae7a3c159b138f8dfd6e09634c1e2a7714e
wuwei <wangfl>
2020-08-25 14:26:56 +0800

    demo第三版

--- src/main/java/com/example/demo/until/HttpClientUntil.java
+++ src/main/java/com/example/demo/until/HttpClientUntil.java
@@ -1,84 +1,172 @@
 package com.example.demo.until;
 
-import com.alibaba.fastjson.JSONObject;
-import com.example.demo.entity.MyEntity;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-import org.apache.logging.log4j.util.Strings;
-
 import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.junit.platform.commons.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.text.ParseException;
+/**
+ * @ClassName: HttpClientUtil
+ * @Description: TODO
+ * @author Mundo
+ */
 
-public class HttpClientUntil {
+public class HttpClientUtil {
+    private static final Logger logger = LoggerFactory.getLogger(HttpClientUtil.class);
 
-    public static CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+    /**
+     * @Title: doGet
+     * @Description: get方式
+     * @param url请求路径
+     * @param params参数
+     * @author Mundo
+     */
+    public static String doGet(String url, Map<String, String> params) {
 
-    public static <T>Object get(String url, Class<T> t){
-        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
-
-        //String mailNo="9862120028918";
-        // 参数
-       // StringBuffer params = new StringBuffer();
-        // 字符数据最好encoding以下;这样一来，某些特殊字符才能传过去(如:某人的名字就是“&”,不encoding的话,传不过去)
-       // params.append("mailNo="+mailNo/*+"&spellName=&exp-textName=&tk=312ebcf5&tm=1598243961139&callback=_jqjsp&_1598243961140="*/);
-
-        // 创建Get请求
-       // HttpGet httpGet = new HttpGet("https://biz.trace.ickd.cn/auto/" + mailNo+"?" + params);
-        String s = null;
-        HttpGet httpGet = new HttpGet(url);
-        // 响应模型
-        CloseableHttpResponse response = null;
+        // 返回结果
+        String result = "";
+        // 创建HttpClient对象
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet httpGet = null;
         try {
-            // 配置信息
-            RequestConfig requestConfig = RequestConfig.custom()
-                    // 设置连接超时时间(单位毫秒)
-                    .setConnectTimeout(5000)
-                    // 设置请求超时时间(单位毫秒)
-                    .setConnectionRequestTimeout(5000)
-                    // socket读写超时时间(单位毫秒)
-                    .setSocketTimeout(5000)
-                    // 设置是否允许重定向(默认为true)
-                    .setRedirectsEnabled(true).build();
-
-            // 将上面的配置信息 运用到这个Get请求里
-            httpGet.setConfig(requestConfig);
-
-            // 由客户端执行(发送)Get请求
-            response = httpClient.execute(httpGet);
-
-            // 从响应模型中获取响应实体
-            HttpEntity responseEntity = response.getEntity();
-            System.out.println("响应状态为:" + response.getStatusLine());
-            if (responseEntity != null) {
-                s = EntityUtils.toString(responseEntity);
+            // 拼接参数,可以用URIBuilder,也可以直接拼接在？传值，拼在url后面，如下--httpGet = new
+            // HttpGet(uri+"?id=123");
+            URIBuilder uriBuilder = new URIBuilder(url);
+            if (null != params && !params.isEmpty()) {
+                for (Map.Entry<String, String> entry : params.entrySet()) {
+                    uriBuilder.addParameter(entry.getKey(), entry.getValue());
+                    // 或者用
+                    // 顺便说一下不同(setParameter会覆盖同名参数的值，addParameter则不会)
+                    // uriBuilder.setParameter(entry.getKey(), entry.getValue());
+                }
             }
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            URI uri = uriBuilder.build();
+            // 创建get请求
+            httpGet = new HttpGet(uri);
+            logger.info("访问路径：" + uri);
+            HttpResponse response = httpClient.execute(httpGet);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {// 返回200，请求成功
+                // 结果返回
+                result = EntityUtils.toString(response.getEntity());
+                logger.info("请求成功！，返回数据：" + result);
+            } else {
+                logger.info("请求失败！");
+            }
+        } catch (Exception e) {
+            logger.info("请求失败!");
+            logger.error(ExceptionUtils.getStackTrace(e));
+        } finally {
+            // 释放连接
+            if (null != httpGet) {
+                httpGet.releaseConnection();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @Title: doPost
+     * @Description: post请求
+     * @param url
+     * @param params
+     * @return
+     * @author Mundo
+     */
+    public static String doPost(String url, Map<String, String> params) {
+        String result = "";
+        // 创建httpclient对象
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost httpPost = new HttpPost(url);
+        try { // 参数键值对
+            if (null != params && !params.isEmpty()) {
+                List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+                NameValuePair pair = null;
+                for (String key : params.keySet()) {
+                    pair = new BasicNameValuePair(key, params.get(key));
+                    pairs.add(pair);
+                }
+                // 模拟表单
+                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(pairs);
+                httpPost.setEntity(entity);
+            }
+            HttpResponse response = httpClient.execute(httpPost);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                result = EntityUtils.toString(response.getEntity(), "utf-8");
+                logger.info("返回数据：>>>" + result);
+            } else {
+                logger.info("请求失败！，url:" + url);
+            }
+        } catch (Exception e) {
+            logger.error("请求失败");
             e.printStackTrace();
         } finally {
-            try {
-                // 释放资源
-                if (httpClient != null) {
-                    httpClient.close();
-                }
-                if (response != null) {
-                    response.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (null != httpPost) {
+                // 释放连接
+                httpPost.releaseConnection();
             }
         }
-        if(Strings.isNotEmpty(s)){
-           return JSONObject.parseObject(s,t);
+        return result;
+    }
+
+    /**
+     * @Title: sendJsonStr
+     * @Description: post发送json字符串
+     * @param url
+     * @param params
+     * @return 返回数据
+     * @author Mundo
+     */
+    public static String sendJsonStr(String url, String params) {
+        String result = "";
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost httpPost = new HttpPost(url);
+        try {
+            httpPost.addHeader("Content-type", "application/json; charset=utf-8");
+            httpPost.setHeader("Accept", "application/json");
+            if (StringUtils.isNotBlank(params)) {
+                httpPost.setEntity(new StringEntity(params, Charset.forName("UTF-8")));
+            }
+            HttpResponse response = httpClient.execute(httpPost);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                result = EntityUtils.toString(response.getEntity());
+                logger.info("返回数据：" + result);
+            } else {
+                logger.info("请求失败");
+            }
+        } catch (IOException e) {
+            logger.error("请求异常");
         }
-        return null;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("id", UUID.randomUUID().toString());
+        String get = doGet("http://localhost:8080/mundo/test", map);
+        System.out.println("get请求调用成功，返回数据是：" + get);
+        String post = doPost("http://localhost:8080/mundo/test", map);
+        System.out.println("post调用成功，返回数据是：" + post);
+        String json = sendJsonStr("http://localhost:8080/mundo/test", "{\"name\":\"David\"}");
+        System.out.println("json发送成功，返回数据是：" + json);
     }
 }
